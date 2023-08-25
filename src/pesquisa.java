b6dad49f0527a2f6c09f537d9e501a2cb51b0fa8
Vinícius Portes <141188979+vportes@users.noreply.github.com>
2023-08-25 20:33:53 -0300

    segundo commit

--- src/pesquisa.java
+++ src/pesquisa.java
@@ -4,10 +4,9 @@ public class pesquisa {
 
     static Scanner teclado = new Scanner(System.in);
     static int pesquisa[] = new int[20];
-    public static int total1, total2, total3, total4, total5;
+    static int total[] = new int[5];
 
     public static void inserirVoto() {
-        total1 = total2 = total3 = total4 = total5 = 0;
         System.out.println("Avaliação da cantina! Avalie numa escala entre 1 (péssimo) e 5 (excelente).");
         for (int i = 0; i < pesquisa.length; i++) {
             int voto = 0;
@@ -17,23 +16,7 @@ public class pesquisa {
                 if (voto < 1 || voto > 5){
                     System.out.println("Voto inválido!");
                 } else{
-                    switch (voto){
-                        case 1:
-                            total1++;
-                            break;
-                        case 2:
-                            total2++;
-                            break;
-                        case 3:
-                            total3++;
-                            break;
-                        case 4:
-                            total4++;
-                            break;
-                        case 5:
-                            total5++;
-                            break;
-                    }
+                    total[(voto-1)]++;
                 }
             }
             pesquisa[i] = voto;
@@ -43,7 +26,7 @@ public class pesquisa {
     public static void mostrarVotos(){
         System.out.println("Mostrar votos:");
         for (int i = 0; i < pesquisa.length; i++){
-            System.out.print(pesquisa[i]+" ");
+            System.out.println("Voto "+(i+1)+" = "+pesquisa[i]);
         }
     }
 
@@ -55,12 +38,19 @@ public class pesquisa {
         System.out.println("Média dos votos: "+soma/ pesquisa.length);
     }
 
+    public static void mostrarTotais(){
+        for (int i = 0; i<total.length; i++){
+            System.out.println(total[i]+" votantes votaram "+(i+1));
+        }
+    }
+
     public static void main(String[] args) {
         int opcao = 0;
         while (opcao != 9) {
             System.out.println("\n1 - Inserir votos");
             System.out.println("2 - Mostrar votos");
             System.out.println("3 - Mostrar média");
+            System.out.println("4 - Mostrar totais");
             System.out.println("9 - Fim");
             System.out.print("Escolha: ");
             opcao = teclado.nextInt();
@@ -73,6 +63,9 @@ public class pesquisa {
                     break;
                 case 3:
                     mostrarMedia();
+                    break;
+                case 4:
+                    mostrarTotais();
                     break;
                 case 9:
                     System.out.println("Fim");
